53dd4fef34aad8a8b71034e0b57dfd9af20c2286
Diamondarms <gui.tonon.03@gmail.com>
2023-12-15 00:51:50 -0300

    3d e teclado

--- openGL codes/Tridimensional.cpp
+++ openGL codes/Tridimensional.cpp
@@ -2,51 +2,53 @@
 #include <gl/gl.h>
 #include <gl/glu.h>
 #include <gl/glut.h>
+#include <valarray>
 
-GLfloat x1 = 0.0f;
-GLfloat y1 = 0.0f;
-GLfloat rsize = 26.0f;
+#define GL_PI 3.1415f
 
-GLfloat xstep = 2.0f;
-GLfloat ystep = 2.0f;
+GLfloat xRot = 0.0f;
+GLfloat yRot = 0.0f;
 
-GLfloat windowWidth;
-GLfloat windowHeight;
+int xMouse = 0;
+int yMouse = 0;
+
+const float step = 1.0f;
+const float maxRotation = 360.0f;
+
+bool mouseButtonPressed = false;
 
 void RenderScene(void){
+    GLfloat x,y,z,angle;
+
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0f,0.0f,0.0f);
-    glRectf(x1, y1, x1+rsize, y1-rsize);
+
+    glPushMatrix();
+    glRotatef(xRot,1.0f,0.0f,0.0f);
+    glRotatef(yRot,0.0f,1.0f,0.0f);
+
+    glBegin(GL_POINTS);
+
+    z = -50.0f;
+    for (angle = 0; angle <= (2.0f*GL_PI)*3.0f; angle += 0.1f)
+    {
+        x = 50.0f*sin(angle);
+        y = 50.0f*cos(angle);
+
+        glVertex3f(x,y,z);
+        z+=0.5f;
+    }
+        
+    glEnd();
+
+    glPopMatrix();
+
     glutSwapBuffers();
 }
 
 void SetupRC(void) {
-    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
-}
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
-void TimerFunction(int value){
-    if(x1 > windowWidth-rsize || x1 < -windowWidth){
-        xstep = -xstep;
-    }
-
-    if(y1 > windowHeight || y1 < -windowHeight+rsize)
-        ystep = -ystep;
-
-    x1 += xstep;
-    y1 += ystep;
-
-    if(x1 > (windowWidth-rsize+xstep))
-        x1 = windowWidth-rsize-1;
-    else if(x1 < -(windowWidth + xstep))
-    x1 = -windowWidth-1;
-
-    if(y1 > (windowHeight+ystep))
-        y1 = windowHeight-1;
-    else if(y1 < -(windowHeight-rsize + ystep))
-    y1 = -windowHeight+rsize-1;
-
-    glutPostRedisplay();
-    glutTimerFunc(3,TimerFunction,1);
+    glColor3f(0.0f,1.0f,0.0f);
 }
 
 void ChangeSize(GLsizei w, GLsizei h){
@@ -69,6 +71,67 @@ void ChangeSize(GLsizei w, GLsizei h){
     glLoadIdentity();
 }
 
+void Keys(unsigned char key, int x, int y) {
+    switch (key) {
+        case 27:  // Código ASCII para a tecla Esc
+            exit(0); 
+            break;
+    }
+}
+
+void SpecialKeys(int key, int x, int y){
+    switch (key) {
+        case GLUT_KEY_UP:
+            xRot += step;
+            break;
+        case GLUT_KEY_DOWN:
+            xRot -= step;
+            break;
+        case GLUT_KEY_LEFT:
+            yRot -= step;
+            break;
+        case GLUT_KEY_RIGHT:
+            yRot += step;
+            break;
+    }
+
+    if (xRot >= maxRotation) xRot -= maxRotation;
+    if (yRot >= maxRotation) yRot -= maxRotation;
+
+    switch (key) {
+        case 27:  // Código ASCII para a tecla Esc
+            exit(0);  // Encerra o programa
+            break;
+    }
+    glutPostRedisplay();
+}
+
+void Mouse(int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON) {
+        if (state == GLUT_DOWN) {
+            mouseButtonPressed = true;
+            xMouse = x;
+            yMouse = y;
+        } else {
+            mouseButtonPressed = false;
+        }
+    }
+}
+
+void Motion(int x, int y) {
+    if (mouseButtonPressed) {
+        xRot += x > xMouse ? step : x < xMouse ? -step : 0;
+        xMouse = x;
+        yRot += y > yMouse ? step : y < yMouse ? -step : 0;
+        yMouse = y;
+
+        if (xRot >= maxRotation) xRot -= maxRotation;
+        if (yRot >= maxRotation) yRot -= maxRotation;
+
+        glutPostRedisplay();
+    }
+}
+
 int main(int argc, char* argv[]){
 
     glutInit(&argc, argv);
@@ -77,10 +140,14 @@ int main(int argc, char* argv[]){
     glutCreateWindow("Banana");
     glutDisplayFunc(RenderScene);
     glutReshapeFunc(ChangeSize);
-    glutTimerFunc(3,TimerFunction,1);
+    glutKeyboardFunc(Keys);
+    glutSpecialFunc(SpecialKeys);
+    glutMouseFunc(Mouse);
+    glutMotionFunc(Motion);
     SetupRC();
 
     glutMainLoop();
 
     return 0;
 }
+
